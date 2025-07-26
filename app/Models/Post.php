f19a8f83d1cf0d424da6f9484e10c431d269283c
Omar Saiouf <homar0083@gmail.com>
2025-07-26 17:17:10 +0300

    Add the design

---
+++ app/Models/Post.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+
+class Post extends Model
+{
+    use HasUuids;
+    use HasFactory, Notifiable;
+    protected $guarded = ['id'];
+}
